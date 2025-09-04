c301228a1f14cc8dd5251e106fa361d373e1cdf7
Vitaly Vakhteev <vitaly.v@techusage.com>
2025-09-04 11:22:01 -0700

    Base files

---
+++ src/test/java/com/picnic/potluck/PotluckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picnic.potluck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotluckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
